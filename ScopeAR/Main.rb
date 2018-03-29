6ec9491ede8f8e219bd8c2fc5b3d027e1c7e8ef3
hrmr <haleyrequa@gmail.com>
2018-03-28 20:40:37 -0600

    Added more comments to Main file

--- ScopeAR/Main.rb
+++ ScopeAR/Main.rb
@@ -1,4 +1,8 @@
 
+# This main ruby file creates a Christmas Tree class Object, and and X class Object.
+# It class prompt and draw methods from these classes, to essentially draw
+# an X and a Christmas tree out of text, with the users choice of height.
+
 require './christmas_tree' # Christmas tree class draws a tree out of text
 require './x' # X class draws an X out of text
 
